f879aa58e477681b737b501315d51b77b0cf5204
straight28 <poem32@naver.com>
2019-11-13 15:57:31 +0900

    2019.11.13 15:57

---
+++ src/ConsoleExcelToPdfTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using Spire.Pdf;
+using Spire.Xls;
+using System;
+
+namespace ConsoleExcelToPdfTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Excel 생성 Closed.xml
+            //using(var workbook = new XLWorkbook())
+            //{
+            //    var worksheet = workbook.Worksheets.Add("sample sheet");
+            //    worksheet.Cell("A1").Value = "안뇽하세요";
+            //    worksheet.Cell("C1").FormulaA1 = "=MID(A1, 7, 5)";
+            //    workbook.SaveAs("test.xlsx");
+            //}
+
+            //이상하게 뽑힘 FreeSpire.xml
+            //Workbook workbook = new Workbook();
+            //workbook.LoadFromFile(@"C:\Users\smkwon\Downloads\.xlsx");
+            //workbook.SaveToFile(".pdf", Spire.Xls.FileFormat.PDF);
+
+
+
+
+
+        }
+    }
+}
