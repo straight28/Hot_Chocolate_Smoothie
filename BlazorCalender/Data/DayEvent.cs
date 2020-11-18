1973cff62d5c044ae0432f90c7c61f34513f3c94
베르베제 <poem32@naver.com>
2020-11-18 17:48:40 +0900

    2020.11.18 Test Session

---
+++ BlazorCalender/Data/DayEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BlazorCalender.Data
+{
+    public class DayEvent
+    {
+        public int DayEventId { get; set; }
+        public string Note { get; set; }
+
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+
+        public DateTime FromDate { get; set; } = DateTime.MinValue;
+
+        public DateTime ToDate { get; set; } = DateTime.MinValue;
+
+        public string DateValue { get; set; }
+
+        public string DayName { get; set; }
+        public string Message { get; set; }
+
+
+
+    }
+}
---
+++ BlazorCalender/Data/WeekClass.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorCalender.Data
+{
+    public class WeekClass
+    {
+        public List<DayEvent> Dates { get; set; } = new List<DayEvent>();
+    }
+}
