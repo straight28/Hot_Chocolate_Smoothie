8815076c7b329511cdab6cf8b635b40bf7ff252c
straight28 <poem32@naver.com>
2021-02-01 18:05:49 +0900

    2021-02-01 cpp init

---
+++ src/cppProject/ex1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	printf(", World ! \n");
+	printf("%d + %d = %d\n ", 2, 3, 5);
+	printf("%f\n",3.14);
+	printf("%.2f\n", 3.14343234);
+	printf("%g\n", 3.14343234);
+	printf("%.3g\n", 32342345.14343234);
+
+	printf("%c %c %c\n", 'a','b','c');
+	printf("%s \n", "æ»≥Á«œººø‰ §æ");
+
+}
