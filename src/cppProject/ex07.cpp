a612efeef17da811167cd30fcb77a467c6535592
straight28 <poem32@naver.com>
2021-02-02 17:21:53 +0900

    2021-02-02

--- src/cppProject/ex07.cpp
+++ src/cppProject/ex07.cpp
@@ -5,9 +5,9 @@
 #include <stdio.h>
 
 int main() {
-	int a, b;
+	// int a, b;
+	float c, d;
 
-	scanf("%d%d", &a, &b); 
 	// scanf 는 보안상의 문제가 있음.
 	// 대신 scanf_s를 사용하게 함.
 	// scanf 로 쓰려면 최상단에 아래와 같이 정의함
@@ -16,16 +16,40 @@ int main() {
 	// #define _CRT_SECURE_NO_WARINGS 을 최상단에 정의 해야함
 	// #endif
 
-	int hap = a + b;
-	int cha = a - b;
-	int gop = a * b;
-	int mok = a / b;
-	int namuji = a % b;
+	// scanf("%d%d", &a, &b);
+	// int hap = a + b;
+	// int cha = a - b;
+	// int gop = a * b;
+	// int mok = a / b;
+	// int namuji = a % b;
+	// 
+	// printf("%d + %d = %d\n", a, b, hap);
+	// printf("%d - %d = %d\n", a, b, cha);
+	// printf("%d * %d = %d\n", a, b, gop);
+	// printf("%d / %d = %d\n", a, b, mok);
 
-	printf("%d + %d = %d\n", a, b, hap);
-	printf("%d - %d = %d\n", a, b, cha);
-	printf("%d * %d = %d\n", a, b, gop);
-	printf("%d / %d = %d\n", a, b, mok);
+	// printf(" ============================ ");
+	// printf(" ============================ ");
+	// printf(" ============================ ");
+
+	scanf("%f%f", &c, &d);
+	// float fhap = c + d;
+	// float fcha = c - d;
+	// float fgop = c * d;
+	// float fmok = c / d;
+
+	// printf("%f + %f = %f\n", c, d, fhap);
+	// printf("%f - %f = %f\n", c, d, fcha);
+	// printf("%f * %f = %f\n", c, d, fgop);
+	// printf("%f / %f = %f\n", c, d, fmok);
+
+	printf("%f + %f = %f\n", c, d, c + d);
+	printf("%f - %f = %f\n", c, d, c - d);
+	printf("%f * %f = %f\n", c, d, c * d);
+	printf("%f / %f = %f\n", c, d, c / d);
+
+
+
 
 }
 
---
+++ src/cppProject/ex08.cpp
@@ -0,0 +1,34 @@
+﻿// #include <stdio.h>
+// 
+// 
+// int main() {
+// 	char a = 65;
+// 
+// 	printf(" %c ", a);
+// }
+
+#include <iostream>
+
+int main() {
+	int i, sum = 0;
+	int user_input;
+
+	for (i = 0; i < 10; i++) {
+		std::cout << i << std::endl;
+		sum += i;
+	}
+
+	std::cout << " 합은 : " << sum << std::endl;
+	
+	std::cout << " 입력 : ";
+
+	std::cin >> user_input;  // cin은 cout처럼 std에 정의되어 있다. 
+
+	std::cout << " 입력값은 " << user_input << std::endl;
+	
+	
+	return 0;
+
+}
+
+
