25b97627117e009ab2bed1509498786f37fa92c8
straight28 <poem32@naver.com>
2021-02-03 18:29:54 +0900

    2021-02-03 18:29 cpp basic

---
+++ src/cppProject/ex18.cpp
@@ -0,0 +1,39 @@
+﻿#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif 
+
+#include <stdio.h>
+
+// 파스칼의 삼각형
+
+/*
+1
+1	1
+1	2	1
+1	3	3	1
+
+j == 0 || j == i -> i
+i행 j열 = [i - 1 j - 1] + [i - 1 j]
+*/
+
+int main() {
+	int p[20][20];
+
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j <= i ; j++)
+		{
+			// 맨 첫 부분 || 맨 끝 부분
+			if (j == 0 || j == i) {
+				p[i][j] = 1;
+			}
+			else {
+				p[19][18] = p[18][17] + p[1][0];
+				p[i][j] = p[i - 1][j - 1] + p[i - 1][j];
+			}
+
+			printf("%d ", p[i][j]);
+		}
+		printf("\n");
+	}
+}
---
+++ src/cppProject/ex19.cpp
@@ -0,0 +1,38 @@
+﻿#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char arr[] = "Hello, world";
+	char str[100] = "Hello";
+
+	// printf("%s\n", arr);
+	
+	printf("배열의 크기 : %d\n ", sizeof(arr) / sizeof(char));
+
+	char str1[] = "Hello";
+	char str2[100];
+
+	// 헤더파일 <string.h>   C++에서는 <cstring>
+	strcpy(str2, str1);
+	// st1에 있는 문자열을 str2로 복사하는 함수
+	printf("str2 : %s\n ", str2);
+
+	// strcat 문자열 연결!
+	strcat(str, "World!");
+	strcat(str, "World!");
+	strcat(str, "World!");
+	strcat(str, "World!");
+	strcat(str, "World!");
+
+	printf("%s\n", str);
+
+
+
+
+
+
+}
