f21e5211be37c836c9714951fa31467cc105dc31
straight28 <poem32@naver.com>
2021-02-10 15:44:29 +0900

    2021-02-10 15:44 cpp basic

--- src/ConsoleApplication1/cpptest_210209_literal.cpp
+++ src/ConsoleApplication1/cpptest_210209_literal.cpp
@@ -6,18 +6,18 @@
 
  #pragma region + Code
 
-//  int main() {
-//  	char str[] = "literal";
-//  	const char* pstr = "literal";
-//  
-//  	printf("str : %s \n", str);
-//  	printf("pstr : %s \n", pstr);
-//  
-//  	printf("pstr : %d \n", "literal");
-//  
-//  	return 0;
-//  }
-//
+int main() {
+	char str[] = "literal";
+	const char* pstr = "literal";
+
+	printf("str : %s \n", &str);
+	printf("pstr : %s \n", pstr);
+
+	printf("pstr : %d \n", "literal");
+
+	return 0;
+}
+
 
 #pragma endregion
 
@@ -48,39 +48,40 @@
 //		널 문자가 들어갈 공간이 있어야 하기 때문에 3 글자라고 해도, 배열은 4칸이 필요하게 된다.
 
 
-int main() 
-{
-	char sentence_1[5] = { 'T','E','S','T', '\0' };
-	char sentence_2[5] = { 'T','E','S','T', 0 };
-	char sentence_3[5] = { 'T','E','S','T', (char)NULL };
-	char sentence_4[5] = { "TEST" };
-
-	printf("sentence_1 : %s \n", sentence_1);
-	printf("sentence_2 : %s \n", sentence_2);
-	printf("sentence_3 : %s \n", sentence_3);
-	printf("sentence_4 : %s \n", sentence_4);
-
-	// %c는 한 문자만을 출력하는 것이지만, 
-	// %s를 이용한다면 null 이 나올때까지 문자를 계속 출력하게 됩니다.
-
-	char ward[30] = { "long sentence" };
-	char* str = ward;
-
-	printf("%s \n",str);
-	// char* 를 이용하여 char 배열을 가리키고 있다.
-	// 따라서 str이라는 char을 가리키는 포인터가 배열 word를 가리친다.
-	// 즉 str이 가리키는 것을 문자열로 출력한다. 
-
-	int str_length(char* name);
-	char name[] = "What is your name ?";
-
-	printf(" 이 문자열의 길이 : %d \n ", str_length(name));
-
-
-
-	return 0;
-
-}
+// int main() 
+// {
+// 
+// 	char sentence_1[5] = { 'T','E','S','T', '\0' };
+// 	char sentence_2[5] = { 'T','E','S','T', 0 };
+// 	char sentence_3[5] = { 'T','E','S','T', (char)NULL };
+// 	char sentence_4[5] = { "TEST" };
+// 
+// 	printf("sentence_1 : %s \n", sentence_1);
+// 	printf("sentence_2 : %s \n", sentence_2);
+// 	printf("sentence_3 : %s \n", sentence_3);
+// 	printf("sentence_4 : %s \n", sentence_4);
+// 
+// 	// %c는 한 문자만을 출력하는 것이지만, 
+// 	// %s를 이용한다면 null 이 나올때까지 문자를 계속 출력하게 됩니다.
+// 
+// 	char ward[30] = { "long sentence" };
+// 	char* str = ward;
+// 
+// 	printf("%s \n",str);
+// 	// char* 를 이용하여 char 배열을 가리키고 있다.
+// 	// 따라서 str이라는 char을 가리키는 포인터가 배열 word를 가리친다.
+// 	// 즉 str이 가리키는 것을 문자열로 출력한다. 
+// 
+// 	int str_length(char* name);
+// 	char name[] = "What is your name ?";
+// 
+// 	printf(" 이 문자열의 길이 : %d \n ", str_length(name));
+// 
+// 
+// 
+// 	return 0;
+// 
+// }
 
 
 
